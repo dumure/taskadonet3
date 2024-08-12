2ca230e87e1d99f8c9a2873495b768c28b3ed6de
user <user@DESKTOP-38PKG31>
2024-08-12 23:30:33 +0400

    *

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(121, 19);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(274, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(22, 59);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(373, 232);
+            dataGridView1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(comboBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(423, 319);
+            panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(22, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 30);
+            label1.TabIndex = 2;
+            label1.Text = "Author:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(423, 319);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+        private ComboBox comboBox1;
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,67 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
 namespace taskadonet3
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlConnection;
+        SqlDataAdapter authorsSqlDataAdapter;
+        SqlDataAdapter booksSqlDataAdapter;
+        DataSet authorsDataSet;
+        DataSet booksDataSet;
+        SqlCommandBuilder sqlCommandBuilder;
+        string connectionString = @"Server = (localdb)\MSSQLLocalDB; 
+    Integrated Security = SSPI; Database = Library";
+        int id = 1;
         public Form1()
         {
             InitializeComponent();
+            authorsDataSet = new DataSet();
+            booksDataSet = new DataSet();
+            sqlConnection = new SqlConnection(connectionString);
+            string getAuthorsQuery = @"USE [Library];
+
+SELECT FirstName + ' ' + LastName AS FullName, Id
+FROM Authors;";
+            authorsSqlDataAdapter = new SqlDataAdapter(getAuthorsQuery, sqlConnection);
+            sqlCommandBuilder = new SqlCommandBuilder(authorsSqlDataAdapter);
+            authorsSqlDataAdapter.Fill(authorsDataSet, "authors");
+            List<string> authors = new List<string>();
+            foreach (DataRow row in authorsDataSet.Tables["authors"].Rows)
+            {
+                authors.Add(row["FullName"].ToString());
+            }
+            comboBox1.DataSource = authors;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (DataRow row in authorsDataSet.Tables["authors"].Rows)
+            {
+                if (comboBox1.Text == row["FullName"].ToString())
+                {
+                    id = (int)row["Id"];
+                    break;
+                }
+            }
+            UpdateDataGridView();
+        }
+
+        private void UpdateDataGridView()
+        {
+            booksDataSet.Clear();
+            string getBooksByAuthorQuery = @$"USE [Library];
+
+SELECT [Name], [Pages], [YearPress], [Comment], [Quantity] 
+FROM Books
+WHERE Id_Author = {id};";
+            booksSqlDataAdapter = new SqlDataAdapter(getBooksByAuthorQuery, sqlConnection);
+            sqlCommandBuilder = new SqlCommandBuilder(booksSqlDataAdapter);
+            booksSqlDataAdapter.Fill(booksDataSet, "books");
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = booksDataSet.Tables["books"];
         }
     }
 }
